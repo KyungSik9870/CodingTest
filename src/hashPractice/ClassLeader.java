a8c4f4d0f0da1c8d7c1ee53e3dcac041c9140a3c
KyungSik9870 <rudtlr9870@gmail.com>
2021-11-05 02:25:07 +0900

    feat: 해시 알고리즘 연습 (1-4번)

---
+++ src/hashPractice/ClassLeader.java
@@ -0,0 +1,46 @@
+package hashPractice;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+/**
+ * [학급 회장(해쉬)]
+ *
+ * 학급 회장을 뽑는데 후보로 기호 A, B, C, D, E 후보가 등록을 했습니다.
+ * 투표용지에는 반 학생들이 자기가 선택한 후보의 기호(알파벳)가 쓰여져 있으며 선생님은 그 기호를 발표하고 있습니다.
+ * 선생님의 발표가 끝난 후 어떤 기호의 후보가 학급 회장이 되었는지 출력하는 프로그램을 작성하세요.
+ * 반드시 한 명의 학급회장이 선출되도록 투표결과가 나왔다고 가정합니다.
+ *
+ * 15
+ * BACBACCACCBDEDE
+ *
+ * C
+ */
+public class ClassLeader {
+
+	public String solution(String input){
+		String answer = "";
+		int max = 0;
+		HashMap<Character, Integer> map = new HashMap<>();
+		for (char c : input.toCharArray()){
+			map.put(c , map.getOrDefault(c, 0) + 1);
+		}
+
+		for (char c : map.keySet()) {
+			if (map.get(c) > max){
+				max = map.get(c);
+				answer = String.valueOf(c);
+			}
+		}
+
+		return answer;
+	}
+
+	public static void main(String[] args) {
+	    Scanner scanner = new Scanner(System.in);
+	    ClassLeader classLeader = new ClassLeader();
+	    int n = scanner.nextInt();
+	    String input = scanner.next();
+		System.out.println(classLeader.solution(input));
+	}
+}
---
+++ src/hashPractice/SalesType.java
@@ -0,0 +1,65 @@
+package hashPractice;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+/**
+ * [매출액의 종류]
+ *
+ * 현수의 아빠는 제과점을 운영합니다. 현수아빠는 현수에게 N일 동안의 매출기록을 주고 연속된 K일 동안의 매출액의 종류를
+ * 각 구간별로 구하라고 했습니다.
+ *
+ * 만약 N=7이고 7일 간의 매출기록이 아래와 같고, 이때 K=4이면
+ * 20 12 20 10 23 17 10
+ * 각 연속 4일간의 구간의 매출종류는
+ *
+ * 첫 번째 구간은 [20, 12, 20, 10]는 매출액의 종류가 20, 12, 10으로 3이다.
+ * 두 번째 구간은 [12, 20, 10, 23]는 매출액의 종류가 4이다.
+ * 세 번째 구간은 [20, 10, 23, 17]는 매출액의 종류가 4이다.
+ * 네 번째 구간은 [10, 23, 17, 10]는 매출액의 종류가 3이다.
+ *
+ * N일간의 매출기록과 연속구간의 길이 K가 주어지면 첫 번째 구간부터 각 구간별
+ * 매출액의 종류를 출력하는 프로그램을 작성하세요.
+ *
+ * 7 4
+ * 20 12 20 10 23 17 10
+ *
+ * 3 4 4 3
+ */
+public class SalesType {
+
+	public ArrayList<Integer> solution(int n, int k, int[] input) {
+		ArrayList<Integer> answer = new ArrayList<>();
+		HashMap<Integer, Integer> map = new HashMap<>();
+		for (int i = 0; i < k - 1; i++) {
+			map.put(input[i], map.getOrDefault(input[i], 0) + 1);
+		}
+		int pointer = 0;
+		for (int index = k - 1; index < n; index++) {
+			map.put(input[index], map.getOrDefault(input[index], 0) + 1);
+			answer.add(map.size());
+			map.put(input[pointer], map.get(input[pointer]) - 1);
+			if (map.get(input[pointer]) == 0) {
+				map.remove(input[pointer]);
+			}
+			pointer++;
+		}
+
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		SalesType salesType = new SalesType();
+		int n = scanner.nextInt();
+		int k = scanner.nextInt();
+		int[] input = new int[n];
+		for (int i = 0; i < n; i++) {
+			input[i] = scanner.nextInt();
+		}
+		for (int a : salesType.solution(n, k, input)) {
+			System.out.print(a+ " ");
+		}
+	}
+}
