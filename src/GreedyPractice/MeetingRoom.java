beddc52d0b31f75709f974bd196c2de92603f9c6
KyungSik9870 <rudtlr9870@gmail.com>
2021-12-01 17:00:21 +0900

    feat : 그리디 - 회의실배정 & 씨름선수 설명 추가

---
+++ src/GreedyPractice/MeetingRoom.java
@@ -0,0 +1,65 @@
+package GreedyPractice;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+/**
+ * [회의실 배정]
+ *
+ * 한 개의 회의실이 있는데 이를 사용하고자 하는 n개의 회의들에 대하여 회의실 사용표를 만들려고 한다.
+ * 각 회의에 대해 시작시간과 끝나는 시간이 주어져 있고, 각 회의가 겹치지 않게 하면서 회의실을 사용할 수 있는 최대수의 회의를 찾아라.
+ * 단, 회의는 한번 시작하면 중간에 중단될 수 없으며 한 회의가 끝나는 것과 동시에 다음 회의가 시작될 수 있다.
+ *
+ * 5
+ * 1 4
+ * 2 3
+ * 3 5
+ * 4 6
+ * 5 7
+ *
+ * 3
+ */
+public class MeetingRoom {
+	public int solution(ArrayList<Meeting> meetings) {
+		int answer = 0;
+		int min = Integer.MIN_VALUE;
+		Collections.sort(meetings);
+		for (Meeting m : meetings) {
+			if (m.start >= min){
+				answer++;
+				min = m.end;
+			}
+		}
+		return answer;
+	}
+
+	public static void main(String[] args) {
+	    Scanner scanner = new Scanner(System.in);
+	    MeetingRoom meetingRoom = new MeetingRoom();
+	    int n = scanner.nextInt();
+		ArrayList<Meeting> meetings = new ArrayList<>();
+		for (int i = 0; i < n; i++) {
+			meetings.add(new Meeting(scanner.nextInt(), scanner.nextInt()));
+		}
+		System.out.println(meetingRoom.solution(meetings));
+	}
+}
+
+class Meeting implements Comparable<Meeting>{
+	int start;
+	int end;
+
+	public Meeting(int start, int end) {
+		this.start = start;
+		this.end = end;
+	}
+
+	@Override
+	public int compareTo(Meeting o) {
+		if (this.end == o.end) {
+			return this.start - o.start;
+		}
+		return this.end - o.end;
+	}
+}
--- src/GreedyPractice/WrestlingPlayer.java
+++ src/GreedyPractice/WrestlingPlayer.java
@@ -1,15 +1,70 @@
 package GreedyPractice;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
+/**
+ * [씨름선수]
+ *
+ * 현수는 씨름 감독입니다. 현수는 씨름 선수를 선발공고를 냈고, N명의 지원자가 지원을 했습니다.
+ * 현수는 각 지원자의 키와 몸무게 정보를 알고 있습니다.
+ * 현수는 씨름 선수 선발 원칙을 다음과 같이 정했습니다.
+ * “A라는 지원자를 다른 모든 지원자와 일대일 비교해서 키와 몸무게 모두 A지원자 보다 높은(크고, 무겁다) 지원자가
+ * 존재하면 A지원자는 탈락하고, 그렇지 않으면 선발된다.”
+ * N명의 지원자가 주어지면 위의 선발원칙으로 최대 몇 명의 선수를 선발할 수 있는지 알아내는 프로그램을 작성하세요.
+ *
+ * 5
+ * 172 67
+ * 183 65
+ * 180 70
+ * 170 72
+ * 181 60
+ *
+ * 3
+ *
+ * (183, 65), (180, 70), (170, 72) 가 선발됩니다.
+ * (181, 60)은 (183, 65)보다 키와 몸무게 모두 낮기 때문에 탈락이고,
+ * (172, 67)은 (180, 70) 때문에 탈락입니다.
+ */
 public class WrestlingPlayer {
-	public int solution() {
+	public int solution(Player[] players) {
 		int answer = 0;
+		Arrays.sort(players);
+		int max = Integer.MIN_VALUE;
+
+		for (Player p : players) {
+			if(p.weight > max) {
+				max = p.weight;
+				answer++;
+			}
+		}
 
 		return answer;
 	}
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		WrestlingPlayer wrestlingPlayer = new WrestlingPlayer();
+		int n = scanner.nextInt();
+		Player[] players = new Player[n];
+		for (int i = 0; i < n; i++) {
+			players[i] = new Player(scanner.nextInt(), scanner.nextInt());
+		}
+		System.out.println(wrestlingPlayer.solution(players));
+	}
+}
+
+class Player implements Comparable<Player> {
+	int height;
+	int weight;
+
+	public Player(int height, int weight) {
+		this.height = height;
+		this.weight = weight;
+	}
+
+	@Override
+	public int compareTo(Player o) {
+		return o.height - this.height;
 	}
 }
